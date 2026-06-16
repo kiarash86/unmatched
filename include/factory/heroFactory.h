4e25a99081d37adc13e4b65f047d889adb1d85af
krsh <kiavash86habibi@gmail.com>
2026-06-16 23:56:06 +0330

    phase 1 of hero factory

--- include/factory/heroFactory.h
+++ include/factory/heroFactory.h
@@ -1,1 +1,18 @@
+#include <memory>
+#include "../model/hero.h"
+#include <nlohmann/json.hpp>
+#include "../utility/file.h"
+class HeroFactory
+{
+private:
 
+void setStats(const nlohmann::json & stats);
+
+
+
+
+public:
+
+static std::unique_ptr<Hero> create(const std::string & name);
+
+};
--- src/factory/heroFactory.cpp
+++ src/factory/heroFactory.cpp
@@ -1,1 +1,52 @@
+#include "../../include/factory/heroFactory.h"
+
+
+ std::unique_ptr<Hero> HeroFactory::create(const std::string & name)
+{
+    std::unique_ptr<Hero> hero;
+    nlohmann::json stats;
+    if (name == "dracula")
+    {
+     
+
+   stats = load("data/dracula/stats.h");
+        hero->setName(stats["name"]);
+        hero->setImgSource(stats["img"]);
+        hero->setHealth(stats["health"]);
+        hero->setMovement(stats["movement"]);
+        hero->setTypeOfAttack(stats["attackType"]);
+//here we have some repitive code and needed funcs
+
+for (size_t i = 0; i < 3; i++)
+{
+    hero->addSidekick(SidekickFactory("data/dracula/Sidekick/sisters.json"));
+    
+}
+
+        hero->setAbility(AbilityFactory("data/dracula/abilities"));
+        hero-> setDeck(DeckFactory("data/dracula/deck"));
+        
+    }
+    else if (name== "sherlock")
+    {
+        
+
+   stats = load("data/sherlock/stats.h");
+        hero->setName(stats["name"]);
+        hero->setImgSource(stats["img"]);
+        hero->setHealth(stats["health"]);
+        hero->setMovement(stats["movement"]);
+        hero->setTypeOfAttack(stats["attackType"]);
+//here we have some repitive code and needed funcs
+
+        hero->addSidekick(SidekickFactory("data/sherlock/Sidekick/watson.json"));
+        hero->setAbility(AbilityFactory("data/sherlock/abilities"));
+        hero-> setDeck(DeckFactory("data/sherlock/deck"));
+        
+    }
+    return hero;
+}
+
+
+
 
