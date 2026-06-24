5f477571d875c361c9b3fecd060065ec7fc5fae8
krsh <kiavash86habibi@gmail.com>
2026-06-25 02:09:01 +0330

    test condition for checking if our target unit is near the enemy fighters or no

---
+++ include/engine/conditions/isNearEnemyCondition.h
@@ -0,0 +1,23 @@
+#include "condition.h"
+#include "model/fighter.h"
+class IsNearEnemyCondition : public Condition {
+private:
+
+  int distance{};
+
+public:
+  IsNearEnemyCondition( int distance)
+      :  distance(distance){};
+  ~IsNearEnemyCondition();
+  bool check(gameData &gameData, Fighter* fighter ) override {
+    //check distance between this 2
+    if (fighter->getPosition() == gameData.enemy->position)
+    {
+      return true;
+    }
+    
+    return false;
+    
+  }
+};
+ 
