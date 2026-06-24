5ce9d19b6ef71bb8a80fc75f268ff7774fb7578c
krsh <kiavash86habibi@gmail.com>
2026-06-25 02:01:07 +0330

    add value for cards that related with value such as dmg and heal

--- src/factory/deckFactory.cpp
+++ src/factory/deckFactory.cpp
@@ -1,6 +1,6 @@
 #include "../../include/factory/deckFactory.h"
 
-std::vector<std::unique_ptr<Card>> create(const std::string pathFolder) {
+std::vector<std::unique_ptr<Card>> DeckFactory::create(const std::string pathFolder) {
 
   std::vector<std::unique_ptr<Card>> deck;
   for (const auto &crd : listFiles(pathFolder)) {
