e1c6f6cbe61ea7888a4ebf788fa1d1306f19d47a
krsh <kiavash86habibi@gmail.com>
2026-06-25 02:07:30 +0330

    adding query of counting fighters that we decide and it can have conditions to

---
+++ include/engine/queries/countfighter.h
@@ -0,0 +1,28 @@
+#include "engine/conditions/condition.h"
+#include "engine/gameData.h"
+#include "model/typeOfFighter.h"
+#include "query.h"
+class CountFighter : public Query {
+private:
+  TypeOfFighter fighterType;
+
+public:
+  CountFighter(TypeOfFighter type) : fighterType(type){};
+  int get(gameData gameData) override {
+    int value{0};
+    for (auto &&fghr : gameData.board->getFighter(fighterType , gameData.self)) {
+      bool flag = true;
+      for (auto &&cnd : conditions) {
+        if (!cnd->check(gameData, fghr)) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) {
+
+        value++; 
+      }
+    }
+    return value;
+  }
+};
