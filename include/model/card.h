0e184aa738b379f01146c8c84fce98ec749582c2
krsh <kiavash86habibi@gmail.com>
2026-06-25 02:01:45 +0330

    add prototypes of value such as get and set and modify

--- include/model/card.h
+++ include/model/card.h
@@ -1,41 +1,40 @@
 #pragma once
+#include "typeOfCard.h"
+#include "typeOfEvent.h"
+#include "typeOfPerformer.h"
+#include <memory>
 #include <string>
 #include <vector>
-#include<memory>
-#include "typeOfCard.h"
-#include "typeOfPerformer.h"
-#include "typeOfEvent.h"
 class Effect;
-class Card
-{
+class Card {
 private:
-    std::string name;
-    TypeOfCard type;
-    TypeOfPerformer performer;
-    int boost;
-    std::string imgSource;
-    TypeOfEvent eventType;
-    std::vector<std::unique_ptr<Effect>> effects;
-
+  int value;
+  std::string name;
+  TypeOfCard type;
+  TypeOfPerformer performer;
+  int boost;
+  std::string imgSource;
+  TypeOfEvent eventType;
+  std::vector<std::unique_ptr<Effect>> effects;
 
 public:
-
-    int getBoost() const;
-    std::string getName() const;
-    TypeOfCard getCardType() const;
-    TypeOfEvent getEventType() const ;
-    std::vector<std::unique_ptr<Effect>> & getEffects() ;
-    std::string getImgSource() const ;
-    TypeOfPerformer getPerformer() const;
-    void setBoost(const int &);
-    void setName(std::string &);
-    void setCardType(const TypeOfCard &);
-    void setEventType(const TypeOfEvent &);
-    void setEffects(const std::vector<Effect> &);
-    void setImgSource(const std::string &);
-    void setPerformer(const TypeOfPerformer &);
-    Card();
-    ~Card()= default;
+  int getValue() const;
+  void setValue(const int&);
+  void modifyValue(const int &);
+  int getBoost() const;
+  std::string getName() const;
+  TypeOfCard getCardType() const;
+  TypeOfEvent getEventType() const;
+  std::vector<std::unique_ptr<Effect>> &getEffects();
+  std::string getImgSource() const;
+  TypeOfPerformer getPerformer() const;
+  void setBoost(const int &);
+  void setName(std::string &);
+  void setCardType(const TypeOfCard &);
+  void setEventType(const TypeOfEvent &);
+  void setEffects(const std::vector<Effect> &);
+  void setImgSource(const std::string &);
+  void setPerformer(const TypeOfPerformer &);
+  Card();
+  ~Card() = default;
 };
-
-
