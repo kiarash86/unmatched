37bbedfea51057547b29f012471377c987d40aa2
krsh <kiavash86habibi@gmail.com>
2026-06-25 02:06:23 +0330

    add a func for adding conditions to a effect

--- include/engine/effects/effect.h
+++ include/engine/effects/effect.h
@@ -1,11 +1,20 @@
+#include<vector>
 #include "../gameData.h"
+#include "../conditions/condition.h"
+#include "engine/queries/query.h"
 class Effect
 {
-private:
-    
+protected:
+    std::vector<std::unique_ptr<Condition>> conditions;
+    std::vector<std::unique_ptr<Query>> queries;
+
 public:
     Effect();
+    void addCondtion(std::unique_ptr<Condition> cond)
+    {
+        conditions.push_back(cond);
+    }
    virtual ~Effect() = default;
-   virtual void excute(gameData& gameData) = 0;
+   virtual void execute(gameData& gameData) = 0;
 };
 
