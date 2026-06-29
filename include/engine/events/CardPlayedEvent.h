ed741f95224ef013ddc3bd0c0111b0ce014d94ae
krsh <kiavash86habibi@gmail.com>
2026-06-29 15:16:42 +0330

    nothing at all

--- include/engine/events/CardPlayedEvent.h
+++ include/engine/events/CardPlayedEvent.h
@@ -3,8 +3,8 @@
 #include "model/hero.h"
 class CardPlayedEvent : public Event {
     public:
-    //FIXME:  no player model yet
-    
+    //FIXME:  no player model yet and probably it shouldnt work with player
+
     Player * player ;
     CardPlayedEvent( Player *  p) : player(p){};
 };
