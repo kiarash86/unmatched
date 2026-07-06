111aacb2f59ca4ceb9418433fad2ff8e8b892b49
krsh <kiavash86habibi@gmail.com>
2026-07-06 12:09:37 +0330

    changing from toggle fullscreen to another full screen solution

---
+++ include/controller/app.h
@@ -0,0 +1,56 @@
+#include "controller/AudioManager.h"
+#include "controller/FontManager.h"
+#include "controller/SceneManager.h"
+#include "controller/TextureManager.h"
+#include "view/enums/ScenesType.h"
+#include <memory>
+class app {
+private:
+  std::unique_ptr<SceneManager> sceneManager;
+  std::unique_ptr<AudioManager> audioManager;
+  std::unique_ptr<FontManager> fontManager;
+  std::unique_ptr<TextureManager> textureManager;
+
+public:
+  void run();
+  app();
+  ~app();
+};
+
+app::app() {
+SetConfigFlags(
+    FLAG_WINDOW_RESIZABLE |
+    FLAG_WINDOW_UNDECORATED);  InitWindow(1980, 1080, "UNMATCHED");
+
+SetWindowPosition(0, 0);
+
+SetWindowSize(
+    GetMonitorWidth(GetCurrentMonitor()),
+    GetMonitorHeight(GetCurrentMonitor()));
+  InitAudioDevice();
+  SetTargetFPS(60);
+  audioManager = std::make_unique<AudioManager>();
+  fontManager = std::make_unique<FontManager>();
+  textureManager = std::make_unique<TextureManager>();
+  sceneManager = std::make_unique<SceneManager>(audioManager.get() ,textureManager.get() , fontManager.get());
+  sceneManager->changeScene(ScenesType::mainScene);
+}
+
+app::~app() {
+  sceneManager.reset();
+  audioManager.reset();
+  fontManager.reset();
+  textureManager.reset();
+  CloseAudioDevice();
+  CloseWindow();
+}
+
+void app::run() {
+  while (!WindowShouldClose()) {
+    sceneManager->update();
+    audioManager->update();
+    BeginDrawing();
+    sceneManager->draw();
+    EndDrawing();
+  }
+}
