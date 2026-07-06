b0745934a52c7f1f2d9c46208b9e3714b3cff72b
krsh <kiavash86habibi@gmail.com>
2026-07-06 12:06:21 +0330

    nothing

---
+++ test_menu.cpp
@@ -0,0 +1,17 @@
+#include "raylib.h"
+#include "view/enums/ScenesType.h"
+#include "view/enumButtonsOFMenuWelcome.h"
+#include "view/scenes/HeroesSelectionScene.h"
+#include "view/scenes/MainScene.h"
+#include "controller/SceneManager.h"
+#include "controller/app.h"
+int main() {
+
+  app app;
+  app.run();
+
+
+
+ 
+}
+
