9638ee87aaa84b8524e48c91ba13eeb801197f85
krsh <kiavash86habibi@gmail.com>
2026-07-21 10:14:25 +0330

    add sidekick, getability and set to hero model

--- include/model/hero.h
+++ include/model/hero.h
@@ -1,38 +1,30 @@
 #pragma once
-#include "typeOfAttack.h"
-#include "ability.h"
-#include "sidekick.h"
-#include "deck.h"
+#include "fighter.h"
 #include <memory>
 #include <vector>
-#include<string>
-#include "fighter.h"
+
 class Deck;
 class Sidekick;
 class Ability;
 
-class Hero : public Fighter
-{
+class Hero : public Fighter {
 private:
-   std::vector< std::unique_ptr<Sidekick>> sidekicks;
-    std::unique_ptr<Deck> deck;
-    std::unique_ptr<Ability> ability;
-
+  std::vector<std::unique_ptr<Sidekick>> sidekicks;
+  std::unique_ptr<Deck> deck;
+  std::unique_ptr<Ability> ability;
 
 public:
+  const std::vector<std::unique_ptr<Sidekick>> &getSidekicks() const;
+  void addSidekick(std::unique_ptr<Sidekick> sidekick);
 
-    const std::vector<std::unique_ptr<Sidekick>>& getSidekicks() const;
-    void addSidekick(std::unique_ptr<Sidekick>& sidekick);
-        Ability * getAbility() const;
-    Deck * getDeck() const;
+  Ability *getAbility() const;
+  void setAbility(std::unique_ptr<Ability> newAbility);
 
+  Deck *getDeck() const;
+  void setDeck(std::unique_ptr<Deck> newDeck);
 
+  Hero();
+  ~Hero() override;
 
-
-    Hero();
-    ~Hero()= default;
+  TypeOfFighter getFighterType() const override { return TypeOfFighter::Hero; }
 };
-
-
-//TODO
-// DELETE THINGS THAT ARE ALREADY IN FIGHTER
--- src/model/hero.cpp
+++ src/model/hero.cpp
@@ -1,23 +1,23 @@
-#include "../../include/model/hero.h"
-const std::vector<std::unique_ptr<Sidekick>>& Hero::getSidekicks() const
-    {
-        return sidekicks;
-        
-    }
-        Ability * Hero::getAbility() const
-        {
-            return ability.get();
-        }
-        Deck * Hero::getDeck() const
-        {
-            return deck.get();
-        }
-    Hero::Hero() {}
- 
+#include "model/hero.h"
+#include "model/sidekick.h"
+#include "model/deck.h"
+#include "model/ability.h"
 
-        void Hero::addSidekick(std::unique_ptr<Sidekick>& sidekick)
-        {
-            sidekicks.push_back(sidekick);
-        }
+Hero::Hero() = default;
+Hero::~Hero() = default;
 
+const std::vector<std::unique_ptr<Sidekick>> &Hero::getSidekicks() const {
+  return sidekicks;
+}
 
+void Hero::addSidekick(std::unique_ptr<Sidekick> sidekick) {
+  sidekicks.push_back(std::move(sidekick));
+}
+
+Ability *Hero::getAbility() const { return ability.get(); }
+void Hero::setAbility(std::unique_ptr<Ability> newAbility) {
+  ability = std::move(newAbility);
+}
+
+Deck *Hero::getDeck() const { return deck.get(); }
+void Hero::setDeck(std::unique_ptr<Deck> newDeck) { deck = std::move(newDeck); }
