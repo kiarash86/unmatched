1a77292242eda718495e748fe318321f3677d640
krsh <kiavash86habibi@gmail.com>
2026-07-21 11:21:13 +0330

    this a effect for changing value of a effect(for example using boast)

---
+++ include/engine/effects/changeValueEffect.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "effect.h"
+#include "model/card.h"
+
+
+class ChangeValueEffect : public Effect {
+private:
+  bool targetEnemy{false};
+  bool useBoost{false};
+  int flatAmount{0};
+
+public:
+  ChangeValueEffect(bool targetEnemy, bool useBoost, int flatAmount = 0)
+      : targetEnemy(targetEnemy), useBoost(useBoost), flatAmount(flatAmount) {}
+  ~ChangeValueEffect() override = default;
+
+  void execute(gameData &gameData) override {
+    if (!conditionsMet(gameData)) return;
+
+    Card *targetCard = targetEnemy ? gameData.enemyCardPlayed : gameData.cardPlayed;
+    if (!targetCard) return;
+
+    int amount = useBoost
+                     ? (gameData.cardPlayed ? gameData.cardPlayed->getBoost() : 0)
+                     : (flatAmount + sumQueries(gameData));
+
+    targetCard->modifyValue(amount);
+  }
+};
