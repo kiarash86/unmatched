704921e9b5519764663d6b1e3ce1bd9045329c42
krsh <kiavash86habibi@gmail.com>
2026-07-21 10:39:25 +0330

    just fix to bug

--- src/factory/mapFactory.cpp
+++ src/factory/mapFactory.cpp
@@ -1,16 +1,15 @@
 #include "factory/mapFactory.h"
 
 std::unique_ptr<Map> MapFactory::create(const std::string &nameMap) {
+  std::string path = "data/maps/" + nameMap + ".json";
+  nlohmann::json mp = load(path);
 
-    std::string path = "data/maps/" + nameMap + ".json";
-    nlohmann::json mp = load(path);
   auto m = std::make_unique<Map>();
   m->setName(mp["name"]);
-  for (const auto &t : mp["tiles"])
-  {
-//BUG : AN ERROR THAT IS RELATED TO "CONST"
-    m->addTile(t["id"] , TileFactory::create(t));
+  for (const auto &t : mp["tiles"]) {
+    int id = t["id"];
+    m->addTile(id, TileFactory::create(t));
   }
-  
+
   return m;
 }
