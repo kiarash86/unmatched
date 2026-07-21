d2b31e0d70a9e251e85b4dcd902bbbb74cba2baa
krsh <kiavash86habibi@gmail.com>
2026-07-21 11:19:58 +0330

    works with else if a condiiton factory

--- include/factory/conditionFactory.h
+++ include/factory/conditionFactory.h
@@ -1,16 +1,17 @@
+#pragma once
 #include <memory>
+#include <nlohmann/json.hpp>
 #include "engine/conditions/condition.h"
 #include "engine/conditions/isNearEnemyCondition.h"
-#include <nlohmann/json.hpp>
+#include "engine/conditions/isLossedCondition.h"
+#include "engine/conditions/isSherlockWatsonCardCondition.h"
+#include "engine/conditions/wonTheWarCondition.h"
+#include "engine/conditions/isWinnerSelfCondition.h"
+#include "engine/conditions/nearHeroCondition.h"
 #include "utility/file.h"
-class ConditionFactory
-{
-private:
 
+class ConditionFactory {
 public:
-
-static std::unique_ptr<Condition> create(const std::string & condition);
-
-
-    
+ 
+  static std::unique_ptr<Condition> create(const std::string &condition, int distance = 1);
 };
--- src/factory/conditionFactory.cpp
+++ src/factory/conditionFactory.cpp
@@ -1,11 +1,28 @@
-#include "../../include/factory/conditionFactory.h"
+#include "factory/conditionFactory.h"
 
-std::unique_ptr<Condition> ConditionFactory::create(const std::string &condition) {
-
-    if (condition == "isNearEnemyCondition")
-    {
-
-      auto cnd = std::make_unique<IsNearEnemyCondition>();
-       return cnd;
-    }
+std::unique_ptr<Condition> ConditionFactory::create(const std::string &condition, int distance) {
+  if (condition == "isNearEnemyCondition" || condition == "near_enemy" ||
+      condition == "same_scope_with_enemy") {
+    return std::make_unique<IsNearEnemyCondition>(distance);
+  }
+  if (condition == "near_each_other") {
+   
+    return std::make_unique<IsNearEnemyCondition>(distance);
+  }
+  if (condition == "near_hero") {
+    return std::make_unique<NearHeroCondition>(distance);
+  }
+  if (condition == "isLossed") {
+    return std::make_unique<IsLossedCondition>();
+  }
+  if (condition == "won_the_war") {
+    return std::make_unique<WonTheWarCondition>(distance);
+  }
+  if (condition == "isWinnerSelf") {
+    return std::make_unique<IsWinnerSelfCondition>();
+  }
+  if (condition == "is_sherlock_watson_card") {
+    return std::make_unique<IsSherlockWatsonCardCondition>();
+  }
+  return nullptr;
 }
