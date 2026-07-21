5425733c135f8906fa52d02ef1c2db91fc35f909
krsh <kiavash86habibi@gmail.com>
2026-07-22 01:51:00 +0330

    replacing proximinity condition to factory

--- src/factory/conditionFactory.cpp
+++ src/factory/conditionFactory.cpp
@@ -4,26 +4,25 @@
 std::unique_ptr<Condition> ConditionFactory::create(const std::string &condition, int distance) {
   if (condition == "isNearEnemyCondition" || condition == "near_enemy" ||
       condition == "same_scope_with_enemy") {
-    return std::make_unique<IsNearEnemyCondition>(distance);
+    return std::make_unique<ProximityCondition>(&gameData::enemy, distance);
   }
   if (condition == "near_each_other") {
-
     return std::make_unique<IsTeamAdjacentCondition>(distance);
   }
   if (condition == "near_hero") {
-    return std::make_unique<NearHeroCondition>(distance);
+    return std::make_unique<ProximityCondition>(&gameData::self, distance);
   }
   if (condition == "near_target") {
-    return std::make_unique<IsNearTargetCondition>(distance);
+    return std::make_unique<ProximityCondition>(&gameData::target, distance);
   }
   if (condition == "isLossed") {
-    return std::make_unique<IsLossedCondition>();
+    return std::make_unique<CombatOutcomeCondition>(CombatOutcome::Lost);
   }
   if (condition == "won_the_war") {
-    return std::make_unique<WonTheWarCondition>(distance);
+    return std::make_unique<CombatOutcomeCondition>(CombatOutcome::Won);
   }
   if (condition == "isWinnerSelf") {
-    return std::make_unique<IsWinnerSelfCondition>();
+    return std::make_unique<CombatOutcomeCondition>(CombatOutcome::Won);
   }
   if (condition == "is_sherlock_watson_card") {
     return std::make_unique<IsSherlockWatsonCardCondition>();
