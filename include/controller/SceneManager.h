ec6bdc1817aaeed33f6f4a94dbb00fe40994c732
krsh <kiavash86habibi@gmail.com>
2026-07-22 00:02:54 +0330

    adding scenemanager exception handling

--- include/controller/SceneManager.h
+++ include/controller/SceneManager.h
@@ -1,10 +1,12 @@
 #pragma once // only once for entire prj
 #include "controller/AudioManager.h"
+#include "utility/exceptions.h"               // for errors
 #include "view/enums/ScenesType.h"            // for changing scenes
 #include "view/scenes/GameScene.h"            // board scene
 #include "view/scenes/HeroSelectionScene.h" // selection scene
 #include "view/scenes/MainScene.h"            // start scene
 #include "view/scenes/Scene.h"                // scene
+#include <iostream>                           // for logging recovered errors
 #include <memory>                             // smart pointer
 class SceneManager {
 private:
@@ -17,23 +19,9 @@ private:
   std::unique_ptr<Scene> currentScene;
   // current scene that is being shown
 
-public:
-  SceneManager(AudioManager *audioManager, TextureManager *textureManager,
-               FontManager *fontManager)
-      : audioManager(audioManager), textureManager(textureManager),
-        fontManager(fontManager){};
-  void changeScene(const ScenesType &type) {
-    // first time(start menu) it wont do anything
-    // but for next changing this will do things
-    // before closing scene
-    if (currentScene != nullptr) {
-      currentScene->onExit();
-    }
-    // first time(start menu) it wont do anything
-    // but for next changing this will do things
-    // before closing scene
-
-    // switch for deciding where to go
+  // this func is created for using that in change scene 
+  //so we can find error with more controll over it
+  void buildScene(const ScenesType &type) {
     switch (type) {
     case ScenesType::mainScene:
       currentScene = std::make_unique<MainScene>(audioManager, this,
@@ -53,7 +41,36 @@ public:
           audioManager, this, textureManager, fontManager);
       break;
     }
-    // switch for deciding where to go
+  }
+
+public:
+  SceneManager(AudioManager *audioManager, TextureManager *textureManager,
+               FontManager *fontManager)
+      : audioManager(audioManager), textureManager(textureManager),
+        fontManager(fontManager){};
+  void changeScene(const ScenesType &type) {
+    // first time(start menu) it wont do anything
+    // but for next changing this will do things
+    // before closing scene
+    if (currentScene != nullptr) {
+      currentScene->onExit();
+    }
+    // first time(start menu) it wont do anything
+    // but for next changing this will do things
+    // before closing scene
+
+    // using try catch when we are moving to a new scene
+    try {
+      buildScene(type);
+    } catch (const AppException &e) {
+      std::cerr << "[SceneManager] Failed to enter scene, returning to main menu: "
+                << e.what() << std::endl;
+      if (type == ScenesType::mainScene) {
+  
+        throw;
+      }
+      buildScene(ScenesType::mainScene); // error? go back to menu
+    }
 
     // first things should be done after enetering new scenes
     currentScene->onEnter();
