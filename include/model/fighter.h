79fc231a5985cf8579d71e5aeeeeae25cdfa4240
krsh <kiavash86habibi@gmail.com>
2026-07-21 10:16:09 +0330

    add func for positions and tileId and fatifued for deck empty

--- include/model/fighter.h
+++ include/model/fighter.h
@@ -1,51 +1,59 @@
 #pragma once
 #include "typeOfAttack.h"
-#include "ability.h"
-#include "sidekick.h"
-#include "deck.h"
+#include "model/typeOfFighter.h"
+#include "tile.h"
 #include <memory>
+#include <string>
 #include <vector>
-#include<string>
-class Deck;
-class Sidekick;
-class Ability;
 
-class Fighter
-{
+class Fighter {
 protected:
-    std::string imgSource;
-    std::string name;
-    int health{};
-    int maxHealth{};
-    int movement{};
-    TypeOfAttack typeOfAttack;
+  std::string imgSource;
+  std::string name;
+  int health{};
+  int maxHealth{};
+  int movement{};
+  TypeOfAttack typeOfAttack{};
+  Vector2D position;
+  int currentTileId{-1};
+  int ownerPlayer{-1};
+  // Set when this fighter takes Empty-Deck fatigue damage; cleared at the
+  // start of its owning hero's next turn. Purely a status flag for other
+  // cards/abilities to query -- it has no built-in effect of its own.
+  bool fatigued{false};
 
-    bool canBeHealed() const;
+  bool canBeHealed() const;
 
 public:
-    void setHealth(const int&);
-    int getHealth() const;    
-    void setName(const std::string&);
-    std::string getName() const;
-    void setImgSource(const std::string&);
-    std::string getImgSource() const;
-    void setMovement(const int&);
-    int getMovement()const;
-    TypeOfAttack getTypeOfAttack() const;
-    void setTypeOfAttack(const TypeOfAttack&);
-    const std::vector<std::unique_ptr<Sidekick>>& getSidekicks() const;
-    void addSidekick(std::unique_ptr<Sidekick>& sidekick);
-        Ability * getAbility() const;
-    Deck * getDeck() const;
-    void damage(const int&);
-    void heal(const int&);
-    bool isAlive() const;
+  void setHealth(const int &);
+  int getHealth() const;
+  void setMaxHealth(const int &);
+  int getMaxHealth() const;
+  void setName(const std::string &);
+  std::string getName() const;
+  void setImgSource(const std::string &);
+  std::string getImgSource() const;
+  void setMovement(const int &);
+  int getMovement() const;
+  TypeOfAttack getTypeOfAttack() const;
+  void setTypeOfAttack(const TypeOfAttack &);
 
+  void setPosition(const Vector2D &);
+  Vector2D getPosition() const;
+  void setTileId(const int &);
+  int getTileId() const;
+  void setOwnerPlayer(const int &);
+  int getOwnerPlayer() const;
 
+  void setFatigued(bool);
+  bool isFatigued() const;
 
-    fighter();
-    ~fighter()= default;
+  virtual TypeOfFighter getFighterType() const = 0;
+
+  void damage(const int &);
+  void heal(const int &);
+  bool isAlive() const;
+
+  Fighter();
+  virtual ~Fighter() = default;
 };
-
-
-//TODO : adding cpp of these funcs
--- src/model/fighter.cpp
+++ src/model/fighter.cpp
@@ -1,9 +1,13 @@
-#include "../../include/model/fighter.h"
+#include "model/fighter.h"
 
-bool Fighter::canBeHealed() const { return (isAlive() and health < maxHealth); }
+Fighter::Fighter() {}
+
+bool Fighter::canBeHealed() const { return (isAlive() && health < maxHealth); }
 
 void Fighter::setHealth(const int &newHealth) { health = newHealth; }
 int Fighter::getHealth() const { return health; }
+void Fighter::setMaxHealth(const int &newMax) { maxHealth = newMax; }
+int Fighter::getMaxHealth() const { return maxHealth; }
 void Fighter::setMovement(const int &newMovement) { movement = newMovement; }
 int Fighter::getMovement() const { return movement; }
 TypeOfAttack Fighter::getTypeOfAttack() const { return typeOfAttack; }
@@ -11,21 +15,28 @@ void Fighter::setTypeOfAttack(const TypeOfAttack &newTypeOfAttack) {
   typeOfAttack = newTypeOfAttack;
 }
 
-void Fighter::damage(const int &dmg) {
+void Fighter::setPosition(const Vector2D &pos) { position = pos; }
+Vector2D Fighter::getPosition() const { return position; }
+void Fighter::setTileId(const int &id) { currentTileId = id; }
+int Fighter::getTileId() const { return currentTileId; }
+void Fighter::setOwnerPlayer(const int &player) { ownerPlayer = player; }
+int Fighter::getOwnerPlayer() const { return ownerPlayer; }
 
-  health -= ((health - dmg) < 0) ? health : dmg;
+void Fighter::setFatigued(bool value) { fatigued = value; } // after deck is empty
+bool Fighter::isFatigued() const { return fatigued; }
+
+void Fighter::damage(const int &dmg) {
+  health -= (dmg > health) ? health : dmg;
 }
-void Fighter::heal(const int &heal) {
+void Fighter::heal(const int &amount) {
   if (canBeHealed()) {
-    health += ((health + heal) > maxHealth) ? maxHealth - health : heal;
+    health += ((health + amount) > maxHealth) ? maxHealth - health : amount;
   }
 }
-bool Fighter::isAlive() const { return health; }
+bool Fighter::isAlive() const { return health > 0; }
 void Fighter::setName(const std::string &newName) { name = newName; }
 std::string Fighter::getName() const { return name; }
 void Fighter::setImgSource(const std::string &newImgSource) {
   imgSource = newImgSource;
 }
 std::string Fighter::getImgSource() const { return imgSource; }
-
-Fighter::Fighter() {}
