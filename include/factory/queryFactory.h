c48d0a95717c8933819d4aad1c9eaafd44d418f2
krsh <kiavash86habibi@gmail.com>
2026-07-21 11:13:40 +0330

    3 kind of query, if not nullptr and if had condition. give it conditions

--- include/factory/queryFactory.h
+++ include/factory/queryFactory.h
@@ -1,15 +1,14 @@
+#pragma once
 #include <memory>
 #include <nlohmann/json.hpp>
-#include "../utility/file.h"
+#include "utility/file.h"
 #include "engine/queries/query.h"
-#include"engine/queries/countfighter.h"
+#include "engine/queries/countfighter.h"
+#include "engine/queries/cardBoost.h"
+#include "engine/queries/countRemovedCards.h"
 #include "factory/conditionFactory.h"
-class QueryFactory
-{
-private:
 
+class QueryFactory {
 public:
-
-static std::unique_ptr<Query> create(const nlohmann::json & query);
-
+  static std::unique_ptr<Query> create(const nlohmann::json &query);
 };
--- src/factory/queryFactory.cpp
+++ src/factory/queryFactory.cpp
@@ -1,19 +1,34 @@
-#include "../../include/factory/queryFactory.h"
+#include "factory/queryFactory.h"
+#include "libraries/magic_enum.hpp"
+#include <cctype>
+
+
 
 std::unique_ptr<Query> QueryFactory::create(const nlohmann::json &query) {
-  auto qry = std::make_unique<Query>();
+  std::unique_ptr<Query> qry;
 
-  
-  if (query["name"] == "countFighter")
-  {
-    qry = std::move(std::make_unique<CountFighter>(query["type"]));
-  }
-  
-  for (auto &&cnd : query["conditions"]) {
-    
-    qry->addCondition(std::move(ConditionFactory::create(std::string(cnd))));
+  std::string name = query.value("name", "");
+  if (name == "countFighter") {
+    qry = std::make_unique<CountFighter>(query.value("type", "Fighter"));
+  } else if (name == "cardBoost") {
+    bool useEnemy = query.value("who", "self") == "enemy";
+    qry = std::make_unique<CardBoost>(useEnemy);
+  } else if (name == "countRemovedCards") {
+    bool useEnemy = query.value("who", "self") == "enemy";
+    qry = std::make_unique<CountRemovedCards>(useEnemy);
   }
 
+  if (!qry) {
+    return nullptr; 
+
+  if (query.contains("conditions")) {
+    for (auto &&cnd : query["conditions"]) {
+      auto cond = ConditionFactory::create(cnd.get<std::string>());
+      if (cond) {
+        qry->addCondition(std::move(cond));
+      }
+    }
+  }
 
   return qry;
 }
