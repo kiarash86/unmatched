9bf8c04db83d6208400040222c5afba570f9872e
krsh <kiavash86habibi@gmail.com>
2026-07-21 11:06:03 +0330

    just add values to stats of fighter nothing more

--- include/factory/sidekickFactory.h
+++ include/factory/sidekickFactory.h
@@ -1,9 +1,10 @@
+#pragma once
 #include <memory>
 #include "../model/sidekick.h"
 #include <nlohmann/json.hpp>
 #include "../utility/file.h"
-class SidekickFactory
-{
+
+class SidekickFactory {
 public:
-    static std::unique_ptr<Sidekick>& create(const std::string &path);
+  static std::unique_ptr<Sidekick> create(const std::string &path);
 };
--- src/factory/sidekickFactory.cpp
+++ src/factory/sidekickFactory.cpp
@@ -1,34 +1,25 @@
-#include "../../include/factory/sidekickFactory.h"
+#include "factory/sidekickFactory.h"
 
-// get hero with stats
-// put data in the hero
-namespace
-{
-
-    void setStats(const nlohmann::json &stats, Sidekick *sidekick)
-    {
-
-        // setting data in sidekick
-        sidekick->setName(stats["name"].get<std::string>());
-        sidekick->setImgSource(stats["img"].get<std::string>());
-        sidekick->setHealth(stats["health"].get<int>());
-        sidekick->setMovement(stats["movement"].get<int>());
-        if (stats["attackType"].get<std::string>() == "range")
-            sidekick->setTypeOfAttack(TypeOfAttack::ranged);
-        else if (stats["attackType"].get<std::string>() == "melee")
-            sidekick->setTypeOfAttack(TypeOfAttack::melee);
-    }
-
+namespace {
+void setStats(const nlohmann::json &stats, Sidekick *sidekick) {
+  sidekick->setName(stats.value("name", ""));
+  sidekick->setImgSource(stats.value("img", ""));
+  sidekick->setHealth(stats.value("health", 1));
+  sidekick->setMaxHealth(stats.value("health", 1));
+  // this dataset's sidekick files use "moves" (heroes use "movement")
+  sidekick->setMovement(stats.value("moves", 0));
+  std::string attackType = stats.value("attackType", "melee");
+  if (attackType == "range") {
+    sidekick->setTypeOfAttack(TypeOfAttack::ranged);
+  } else {
+    sidekick->setTypeOfAttack(TypeOfAttack::melee);
+  }
+}
 }
 
-std::unique_ptr<Sidekick>& SidekickFactory::create(const std::string & path)
-{
-
-    auto sidekick = std::make_unique<Sidekick>();
-
-    nlohmann::json jSidekick = load(path);
-
-    setStats(jSidekick, sidekick.get());
-
-    return sidekick;
+std::unique_ptr<Sidekick> SidekickFactory::create(const std::string &path) {
+  auto sidekick = std::make_unique<Sidekick>();
+  nlohmann::json jSidekick = load(path);
+  setStats(jSidekick, sidekick.get());
+  return sidekick;
 }
