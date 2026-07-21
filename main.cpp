0c164a4405ee2e980e12946f238cb465fe3ca0da
krsh <kiavash86habibi@gmail.com>
2026-07-22 00:22:42 +0330

    put app.run in  in try/catch

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include "controller/app.h"
+#include "utility/exceptions.h"
+#include <iostream>
 
 int main() {
-  app application;
-  application.run();
+
+  try {
+    app application;
+    application.run();
+  }  catch (const std::exception &e) {
+    std::cerr << "Unexpected error: " << e.what() << std::endl;
+    return 1;
+  }
+
   return 0;
 }
