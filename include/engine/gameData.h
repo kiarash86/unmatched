207dbaae9773a8c4eeabb98c3099832a7128044a
krsh <kiavash86habibi@gmail.com>
2026-07-21 10:21:41 +0330

    from int and... to become a fully usefill gameData

--- include/engine/gameData.h
+++ include/engine/gameData.h
@@ -1,13 +1,30 @@
-#include "../model/hero.h"
-#include "../model/card.h"
+#pragma once
+#include "model/typeOfEvent.h"
+#include <functional>
+#include <vector>
 
-struct gameData
-{
-    int board;
-    int age;
-    int target;
-    int self;
-    int enemy;
-    Card * cardPlayed;
-    int value;
+class Fighter;
+class Card;
+class Map;
+class Tile;
+
+
+struct gameData {
+  Map *map{nullptr}; //board
+  Fighter *self{nullptr}; // me
+  Fighter *target{nullptr};
+  Fighter *enemy{nullptr};
+  Card *cardPlayed{nullptr};
+  int value{0};
+  TypeOfEvent event{TypeOfEvent::none};
+
+
+  Fighter *lastCombatWinner{nullptr}; // for exchanging position for example
+  Fighter *lastCombatLoser{nullptr};
+
+ 
+  Card *enemyCardPlayed{nullptr}; //def
+
+  std::function<void(std::vector<Tile *>, std::function<void(Tile *)>)> requestTileChoice; // get tile from user but which tiles?
+  // tiles in a same zone? or near fighter? or...?
 };
