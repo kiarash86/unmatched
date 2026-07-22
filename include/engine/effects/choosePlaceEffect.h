d24d3c23644f87ff6f41f4255512edcf4bf0d419
krsh <kiavash86habibi@gmail.com>
2026-07-22 16:49:00 +0330

    final version of choose place

--- include/engine/effects/choosePlaceEffect.h
+++ include/engine/effects/choosePlaceEffect.h
@@ -6,33 +6,69 @@
 #include <algorithm>
 #include <string>
 
-
 class ChoosePlaceEffect : public Effect {
 private:
   std::string who;
 
 public:
-  explicit ChoosePlaceEffect(std::string who = "self") : who(std::move(who)) {}
+  explicit ChoosePlaceEffect(std::string who = "self")
+      : who(std::move(who)) {} // main func of placeEffect
+
   ~ChoosePlaceEffect() override = default;
 
-  void execute(gameData &gameData) override {
-    if (!conditionsMet(gameData)) return;
+  void
+  execute(gameData &gameData,
+          std::function<void()> onDone) override { // the main func of excuting
+    if (!conditionsMet(gameData)) {
+      if (onDone) {
+        onDone();
+      }
+      return;
+    }
 
     Fighter *mover = (who == "target") ? gameData.target : gameData.self;
-    if (!mover || !gameData.map || !gameData.requestTileChoice) return;
+    if (!mover || !gameData.map || !gameData.requestTileChoice) {
+      if (onDone) {
+        onDone();
+      }
+      return;
+    }
 
     int fromTile = gameData.map->getTileIdOf(mover);
+
     Map *map = gameData.map;
 
     std::vector<Tile *> options;
     for (auto &[id, tile] : map->getTiles()) {
-      if (id == fromTile) continue;
-      if (map->isOccupied(id)) continue;
+
+      if (id == fromTile) {
+        continue;
+      }
+      if (map->isOccupied(id)) {
+        continue;
+      }
+
       options.push_back(tile.get());
     }
+    if (options.empty()) {
+      if (onDone) {
 
-    gameData.requestTileChoice(options, [map, mover](Tile *chosen) {
-      if (chosen) map->placeFighter(mover, chosen->getId());
-    });
+        onDone();
+      }
+      return;
+    } // ok nothing to do - bye
+
+    gameData.requestTileChoice(
+        options, [map, mover, onDone](
+                     Tile *chosen) { // get a tile in options and who will be
+                                     // this lucky person to move , when?
+          if (chosen) {
+            map->placeFighter(mover, chosen->getId());
+          }
+          if (onDone) {
+
+            onDone();
+          }
+        });
   }
 };
