5a8f86f6c14d535109a6b2d2b53051d9d727417d
krsh <kiavash86habibi@gmail.com>
2026-07-22 16:40:03 +0330

    deleting  some includes

--- include/factory/conditionFactory.h
+++ include/factory/conditionFactory.h
@@ -2,18 +2,14 @@
 #include <memory>
 #include <nlohmann/json.hpp>
 #include "engine/conditions/condition.h"
-#include "engine/conditions/isNearEnemyCondition.h"
+#include "engine/conditions/proximityCondition.h"
+#include "engine/conditions/combatOutcomeCondition.h"
 #include "engine/conditions/isTeamAdjacentCondition.h"
-#include "engine/conditions/isLossedCondition.h"
 #include "engine/conditions/isSherlockWatsonCardCondition.h"
-#include "engine/conditions/wonTheWarCondition.h"
-#include "engine/conditions/isWinnerSelfCondition.h"
-#include "engine/conditions/nearHeroCondition.h"
-#include "engine/conditions/isNearTargetCondition.h"
 #include "utility/file.h"
 
 class ConditionFactory {
 public:
- 
+
   static std::unique_ptr<Condition> create(const std::string &condition, int distance = 1);
 };
