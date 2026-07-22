8fbeae3835a0dc766ae04436474860eb6f793cee
krsh <kiavash86habibi@gmail.com>
2026-07-22 16:43:39 +0330

    final version of count fighter

--- include/engine/queries/countfighter.h
+++ include/engine/queries/countfighter.h
@@ -5,7 +5,11 @@
 #include "model/fighter.h"
 #include "model/map.h"
 #include "query.h"
-
+// we have some type that we can use this
+// just hero and need to find enemy's hero?
+// all heroes and sidekciks and need to find enemy's?
+// just sidekicks...
+// and same with using enemy heroes and sidekicks
 
 class CountFighter : public Query {
 private:
@@ -24,8 +28,13 @@ public:
     if (!owner) return value;
 
     for (auto &&fghr : gameData.map->getFighter(fighterType, owner->getOwnerPlayer())) {
+      if (fghr == gameData.target) { // we use this because if ravening if 
+        //sister moves herself or others sister it would made them dead
+        // because they were a neighbor with 0 distance
+        continue;
+      }
       bool flag = true;
-      for (auto &&cnd : conditions) {
+      for (auto &&cnd : conditions) { //checking conditions
         if (!cnd->check(gameData, fghr)) {
           flag = false;
           break;
