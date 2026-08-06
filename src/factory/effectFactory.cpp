5111a2482c6300f3f57f49259edd281b9f1c4efa
krsh <kiavash86habibi@gmail.com>
2026-08-06 16:41:00 +0330

    adding new effects to effect factory

--- src/factory/effectFactory.cpp
+++ src/factory/effectFactory.cpp
@@ -17,13 +17,26 @@ std::unique_ptr<Effect> EffectFactory::create(const nlohmann::json &effect) {
 
   try {
   std::unique_ptr<Effect> eff;
+  bool conditionHandledInline = false; 
+                                    
 
   if (type == "modify" || type == "def") {
 
     eff = std::make_unique<ChangeValueEffect>(false, false, baseAmount(effect, "howMuch"));
   } else if (type == "attack" || type == "dmg" || type == "damage") { //  part of attacking and dmg and damage
-    eff = std::make_unique<DmgEffect>(baseAmount(effect, "howMuch"),
-                                       effect.value("toWhat", ""));
+    auto dmgEff = std::make_unique<DmgEffect>(baseAmount(effect, "howMuch"),
+                                               effect.value("toWhat", ""));
+   if (effect.contains("howMuchIfCondition") && effect["howMuchIfCondition"].is_number() &&
+        effect.contains("condition") && effect["condition"].is_string()) {
+      auto boostCond = ConditionFactory::create(effect["condition"].get<std::string>(),
+                                                 effect.value("distance", 1));
+      if (boostCond) {
+        dmgEff->setConditionalValue(std::move(boostCond),
+                                     effect["howMuchIfCondition"].get<int>());
+        conditionHandledInline = true;
+      }
+    }
+    eff = std::move(dmgEff);
   } else if (type == "draw_card" || type == "draw") {
     int amount = baseAmount(effect, "howMany");
     std::string toWho = effect.value("toWho", effect.value("who", "self"));
@@ -47,14 +60,19 @@ std::unique_ptr<Effect> EffectFactory::create(const nlohmann::json &effect) {
                        : -1;
     std::string toWhere = effect.value("toWhere", "none");
     bool allowStay = effect.value("allowStay", false);
-    eff = std::make_unique<MoveEffect>(howMany, whichOne, distance, toWhere, allowStay);
+    std::string performer = effect.value("performer", "self");
+    eff = std::make_unique<MoveEffect>(howMany, whichOne, distance, toWhere, allowStay, performer);
   } else if (type == "remove_card") {
     int amount = baseAmount(effect, "howMany");
-    if (amount == 0) amount = baseAmount(effect, "amount");
-    if (amount == 0) amount = 1; 
+    if (amount == 0) {
+      amount = baseAmount(effect, "amount");
+    }
+    if (amount == 0) {
+      amount = 1;
+    }
 
     bool fromEnemy = effect.value("from", "self") == "enemy";
-    bool userChosen = effect.value("how", "random") == "user_choosen";
+    bool userChosen = effect.value("how", "random") == "user_chosen";
 
     int bonusPerCard = 0;
     if (effect.contains("bonusPerCard") && effect["bonusPerCard"].is_object()) {
@@ -83,8 +101,44 @@ std::unique_ptr<Effect> EffectFactory::create(const nlohmann::json &effect) {
     bool targetEnemy = effect.value("onWho", "") == "enemy";
     bool useBoost = effect.value("changeWith", "") == "boost";
     eff = std::make_unique<ChangeValueEffect>(targetEnemy, useBoost, baseAmount(effect, "changeWith"));
+  } else if (type == "set_value" || type == "set_value_fixed") {
+    bool targetEnemy = effect.value("who", "") == "enemy";
+    eff = std::make_unique<SetValueEffect>(targetEnemy, baseAmount(effect, "howMuch"));
+  } else if (type == "remove_from_board") {
+    eff = std::make_unique<RemoveFromBoardEffect>(effect.value("whichOne", "self"));
+  } else if (type == "place_on_deck") {
+    int amount = baseAmount(effect, "howMany");
+    if (amount == 0) {
+      amount = 1;
+    }
+    bool fromEnemy = effect.value("from", "self") == "enemy";
+    eff = std::make_unique<PlaceOnDeckEffect>(amount, fromEnemy);
+  } else if (type == "choose_effect") {
+    std::vector<std::unique_ptr<Effect>> options;
+    std::vector<std::string> labels;
+    if (effect.contains("options")) {
+      for (auto &&opt : effect["options"]) {
+        if (auto built = EffectFactory::create(opt)) {
+     labels.push_back(opt.value(
+              "label", opt.value("type", opt.value("name", "Option"))));
+          options.push_back(std::move(built));
+        }
+      }
+    }
+    eff = std::make_unique<ChooseEffectEffect>(std::move(options), std::move(labels));
+  } else if (type == "opponent_choice") {
+    std::unique_ptr<Effect> choiceEffect;
+    std::unique_ptr<Effect> declineEffect;
+    if (effect.contains("choice")) {
+      choiceEffect = EffectFactory::create(effect["choice"]);
+    }
+    if (effect.contains("onDecline")) {
+      declineEffect = EffectFactory::create(effect["onDecline"]);
+    }
+    eff = std::make_unique<OpponentChoiceEffect>(std::move(choiceEffect), std::move(declineEffect));
   } else if (type == "choose_place") {
-    eff = std::make_unique<ChoosePlaceEffect>(effect.value("who", "self"));
+    eff = std::make_unique<ChoosePlaceEffect>(effect.value("who", "self"),
+                                               effect.value("when", "immediate"));
   } else if (type == "revive") {
     eff = std::make_unique<ReviveEffect>(effect.value("whichOne", effect.value("who", "sidekick")));
   } else if (type == "predict") {
@@ -95,7 +149,8 @@ std::unique_ptr<Effect> EffectFactory::create(const nlohmann::json &effect) {
     return nullptr; 
   }
 
-  if (effect.contains("condition") && effect["condition"].is_string()) { // adding conditions here
+  if (!conditionHandledInline && effect.contains("condition") &&
+      effect["condition"].is_string()) { // adding conditions here
     auto cond = ConditionFactory::create(effect["condition"].get<std::string>(),
                                           effect.value("distance", 1));
     if (cond) {
