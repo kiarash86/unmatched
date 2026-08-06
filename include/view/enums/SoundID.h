10ae234ceeabbb2b901863a75b846ab784d03592
krsh <kiavash86habibi@gmail.com>
2026-08-06 10:49:47 +0330

    add speech for invibleman

--- include/view/enums/SoundID.h
+++ include/view/enums/SoundID.h
@@ -1,2 +1,2 @@
 #pragma once
-enum class SoundID { drawSword, clickForAll, sherlockSpeech, draculaSpeech , player1Choose , player2Choose };
+enum class SoundID { drawSword, clickForAll, sherlockSpeech, draculaSpeech , invisiblemanSpeech , player1Choose , player2Choose };
