aa3ae0a3edf5c2c7d8f1c51486de65dfa8f260b5
krsh <kiavash86habibi@gmail.com>
2026-08-06 15:55:57 +0330

    adding the ability to choose place with deffered time

--- include/engine/effects/choosePlaceEffect.h
+++ include/engine/effects/choosePlaceEffect.h
@@ -5,27 +5,17 @@
 #include "model/tile.h"
 #include <algorithm>
 #include <string>
-
+// it doesnt have the ability to choose place later
+// so we use a string conditon that give us the time of 
+// choosing it
 class ChoosePlaceEffect : public Effect {
 private:
   std::string who;
 
-public:
-  explicit ChoosePlaceEffect(std::string who = "self")
-      : who(std::move(who)) {} // main func of placeEffect
+  std::string when;
 
-  ~ChoosePlaceEffect() override = default;
 
-  void
-  execute(gameData &gameData,
-          std::function<void()> onDone) override { // the main func of excuting
-    if (!conditionsMet(gameData)) {
-      if (onDone) {
-        onDone();
-      }
-      return;
-    }
-
+  void placeNow(gameData &gameData, std::function<void()> onDone) {
     Fighter *mover = (who == "target") ? gameData.target : gameData.self;
     if (!mover || !gameData.map || !gameData.requestTileChoice) {
       if (onDone) {
@@ -60,8 +50,7 @@ public:
 
     gameData.requestTileChoice(
         options, [map, mover, onDone](
-                     Tile *chosen) { // get a tile in options and who will be
-                                     // this lucky person to move , when?
+                     Tile *chosen) { 
           if (chosen) {
             map->placeFighter(mover, chosen->getId());
           }
@@ -70,5 +59,47 @@ public:
             onDone();
           }
         });
+  }
+
+public:
+  explicit ChoosePlaceEffect(std::string who = "self",
+                              std::string when = "immediate")
+      : who(std::move(who)), when(std::move(when)) {} // main func of placeEffect
+
+  ~ChoosePlaceEffect() override = default;
+
+  void
+  execute(gameData &gameData,
+          std::function<void()> onDone) override { // the main func of excuting
+    if (!conditionsMet(gameData)) {
+      if (onDone) {
+        onDone();
+      }
+      return;
+    }
+
+    Fighter *mover = (who == "target") ? gameData.target : gameData.self;
+    if (!mover) {
+      if (onDone) {
+        onDone();
+      }
+      return;
+    }
+
+    if (when == "start_of_next_turn" && gameData.deferToStartOfNextTurn) {
+    
+      gameData.deferToStartOfNextTurn(mover, this);
+      if (onDone) {
+        onDone();
+      }
+      return;
+    }
+
+   
+    placeNow(gameData, std::move(onDone));
+  }
+
+  void executeDeferred(gameData &gameData, std::function<void()> onDone) override {
+    placeNow(gameData, std::move(onDone));
   }
 };
