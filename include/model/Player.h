cbe95257733acc5eb493175f66a00a720f213b58
YousefPak <yusefpak1386@gmail.com>
2026-08-23 18:47:35 +0330

    Making new for playerclass

--- include/model/Player.h
+++ include/model/Player.h
@@ -1,22 +1,16 @@
 #pragma once
 #include "model/heroList.h"
-#include <string>
 class Player // player
 {
 private:
-  int playerIndex = 0;
   HeroList heroId = HeroList::Dracula;
-  std::string heroName;
 
 public:
   Player() = default;
 
-  Player(int playerIndex, HeroList heroId, const std::string &heroName)
-      : playerIndex(playerIndex), heroId(heroId), heroName(heroName) {}
+Player(HeroList heroId) : heroId(heroId) {}
 
   ~Player() = default;
 
-  int getPlayerIndex() const { return playerIndex; }
   HeroList getHeroId() const { return heroId; }
-  std::string getHeroName() const { return heroName; }
 };
