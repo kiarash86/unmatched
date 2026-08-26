8fceb4897e8022792054a2b5579f3f990387453d
krsh <kiavash86habibi@gmail.com>
2026-08-26 12:43:17 +0330

    adding func for letting ai know that its his turn to choose heroes and sidekicks placement

--- src/controller/AIController.cpp
+++ src/controller/AIController.cpp
@@ -17,3 +17,46 @@ bool AIController::isControlling(const Hero *hero) const
 {
   return hero && hero->getOwnerPlayer() == ownerPlayer;
 }
+
+
+
+bool AIController::pendingBelongsToMe(GameManager &gm) const
+{
+
+  if (gm.isAwaitingHeroStartPlacement())
+  {
+    Hero *h = gm.getHeroAwaitingStartPlacement();
+    return h && isControlling(h);
+  }
+  if (gm.isAwaitingSidekickPlacement())
+  {
+    Fighter *sk = gm.getSidekickAwaitingPlacement();
+    return sk && sk->getOwnerPlayer() == ownerPlayer;
+  }
+  if (gm.isAwaitingFogTokenPlacement())
+  {
+    Hero *h = gm.getFogTokenAwaitingHero();
+    return h && isControlling(h);
+  }
+  if (Fighter *chooser = gm.getPendingChooser())
+  {
+    return chooser->getOwnerPlayer() == ownerPlayer;
+  }
+  if (gm.isCombatActive())
+  {
+    if (Hero *defender = gm.getCombatDefendingHero())
+    {
+      return isControlling(defender);
+    }
+  }
+  Hero *current = gm.getCurrentHero();
+  return current && isControlling(current);
+}
+
+
+
+
+
+
+
+
